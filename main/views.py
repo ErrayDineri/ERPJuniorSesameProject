08ab574e53944561e75d300a981583493222ae4c
Salmen Ben Ammar <40737020+salmen452@users.noreply.github.com>
2025-06-13 09:12:32 +0100

    Templates update_V2

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
@@ -50,3 +50,7 @@ def dashboard_view(request):
             'formations': formations,
             'performances': performances,
         })
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
